1b4d1e4716063a4a9d5cff1bcbcf166986fc016f
Josh Roy <joshnroy@gmail.com>
2021-05-08 07:46:16 -0400

    don't profile with cProfile

--- run_supervised_classifier.sh
+++ run_supervised_classifier.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=1,2 export WANDB_RUN_GROUP=ucf101_supervisedclassifier_64x64 && python -m cProfile -o test.prof supervised_classifier.py --config_file="configs/simsiam_ucf101.yaml" --data_dir="../ucfimages64x64" --log_dir="../logs/ucf101-supervised-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --wandb
+export CUDA_VISIBLE_DEVICES=1,2 export WANDB_RUN_GROUP=ucf101_supervisedclassifier_64x64 && python supervised_classifier.py --config_file="configs/simsiam_ucf101.yaml" --data_dir="../ucfimages64x64" --log_dir="../logs/ucf101-supervised-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --wandb
