7c1ecc55b01a1480fdd48b8961ab31c87dd21025
Josh Roy <joshnroy@gmail.com>
2021-05-12 14:19:39 -0400

    changes for simclr

--- run_local.sh
+++ run_local.sh
@@ -2,10 +2,10 @@
 
 # for k in 0 1 2;
 #     do for i in 0 50;
-# 	    do export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=byol_stream51-cifar_time_jittering_deterministic${i} && python main.py --config_file="configs/byol_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/stream51-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=${i} --preload_dataset --download --wandb;
+# 	    do export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=simclr_stream51-cifar_time_jittering_deterministic${i} && python main.py --config_file="configs/simclr_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/stream51-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=${i} --preload_dataset --download --wandb;
 #     done;
 # done;
 
-for i in 0 1 2 3;
-    do export CUDA_VISIBLE_DEVICES=0 && export WANDB_RUN_GROUP=byol_cifar_sanity && python main.py --config_file="configs/byol_cifar.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --download --wandb;
+for i in 0;
+    do export CUDA_VISIBLE_DEVICES=0 && export WANDB_RUN_GROUP=simclr_cifar_sanity && python main.py --config_file="configs/simclr_cifar.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --download --wandb;
 done;
