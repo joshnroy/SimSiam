25f35dc94e252abf07cdfaee37f4543e47d3fc06
Josh Roy <josh_roy@brown.edu>
2021-05-13 21:46:37 -0400

    changed run group

--- run_local.sh
+++ run_local.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for k in 0 1 2;
+for k in 0 1 2 3;
     do for i in 0 50;
-	    do export CUDA_VISIBLE_DEVICES=1,2 && export WANDB_RUN_GROUP=barlow_stream51-cifar_time_jittering_deterministic${i} && python main.py --config_file="configs/barlow_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/stream51-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=${i} --preload_dataset --download --wandb;
+	    do export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=barlow_stream51-cifar_byolaugs_time_jittering_deterministic${i} && python main.py --config_file="configs/barlow_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/stream51-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=${i} --preload_dataset --download --wandb;
     done;
 done;
 
