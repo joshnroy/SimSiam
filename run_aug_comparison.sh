7cfd4ea8f3d38c5f5efb1006e42ba737ec3085f7
Josh Roy <josh_roy@brown.edu>
2021-05-22 10:40:55 -0400

    modified run scripts

--- run_aug_comparison.sh
+++ run_aug_comparison.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=simsiam_stream51_augmentationcomparison && python main.py --config_file="configs/simsiam_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=0 --preload_dataset --download --single_aug Nothing --wandb;
+# export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=simsiam_stream51_augmentationcomparison && python main.py --config_file="configs/simsiam_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=0 --preload_dataset --download --single_aug Nothing --wandb;
 
-export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=simsiam_stream51_augmentationcomparison && python main.py --config_file="configs/simsiam_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=50 --preload_dataset --download --single_aug Nothing --wandb;
+export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=simsiam_stream51_augmentationcomparison_nobbox && python main.py --config_file="configs/simsiam_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=50 --preload_dataset --download --single_aug Nothing --wandb;
 
 for aug in RandomResizedCrop RandomHorizontalFlip ColorJitter RandomGrayscale;
-    do export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=simsiam_stream51_augmentationcomparison && python main.py --config_file="configs/simsiam_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=0 --preload_dataset --download --single_aug ${aug} --wandb;
+    do export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=simsiam_stream51_augmentationcomparison_nobbox && python main.py --config_file="configs/simsiam_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=0 --preload_dataset --download --single_aug ${aug} --wandb;
 done;
--- run_local.sh
+++ run_local.sh
@@ -7,5 +7,5 @@
 # done;
 
 for i in 50 0;
-    do export CUDA_VISIBLE_DEVICES=0 && export WANDB_RUN_GROUP=simsiam_stream51-cifar_nobbox_deterministic${i} && python main.py --config_file="configs/simsiam_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=${i} --preload_dataset --download --wandb;
+    do export CUDA_VISIBLE_DEVICES=0,1 && export WANDB_RUN_GROUP=simsiam_stream51-cifar_nobbox_deterministic${i} && python main.py --config_file="configs/simsiam_stream51.yaml" --data_dir="../stream_data" --log_dir="../logs/cifar-contrastive-logs-${WANDB_RUN_GROUP}-${ID}/" --ckpt_dir=".cache/${WANDB_RUN_GROUP}" --linear_monitor --temporal_jitter_range=${i} --preload_dataset --download --wandb;
 done;
