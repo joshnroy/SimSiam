594cbc1ae08156d74a933741c9d24259e78193a8
Josh Roy <joshnroy@gmail.com>
2021-06-02 19:59:03 -0400

    moved into more standard src/test format

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name="contrastive-unsupervised",
+    version="0.0.1",
+    author="Josh Roy",
+    author_email="joshnroy@gmail.com",
+    description="An implementation of timeskip with unsupervised representation learning algorithms",
+)
