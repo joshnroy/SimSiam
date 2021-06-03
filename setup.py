76c027a953a69f15211ddb98da230769bd66d234
Josh Roy <joshnroy@gmail.com>
2021-06-02 20:26:40 -0400

    renamed module

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-import setuptools
+from setuptools import setup
+from setuptools import find_packages
 
 
-setuptools.setup(
-    name="contrastive-unsupervised",
+setup(
+    name="contrastive_unsupervised",
     version="0.0.1",
     author="Josh Roy",
     author_email="joshnroy@gmail.com",
     description="An implementation of timeskip with unsupervised representation learning algorithms",
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
 )
